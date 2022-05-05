2455bccd96404b7f088ced816254590b3967746b
IvanSkliar <skliariv91@gmail.com>
2022-05-05 14:26:43 +0300

    05.05.2022

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgContainer = document.querySelector('.gallery');
+
+
+
+const cardsMarkup = createImgCards(galleryItems);  
+imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImgCards(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `
+    }).join('');
+}
+
+
+const gallery = new SimpleLightbox('.gallery a');
+
+gallery.options.captionsData = "alt";
+gallery.options.captionDelay = 250;
